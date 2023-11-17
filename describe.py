2c1be17cbb6198ce57f92caf699299f6c42ed0e9
Hugo CARPENTIER <hcarpent@made-f0Ar11s4.clusters.42paris.fr>
2023-11-17 22:56:22 +0100

    shorter

--- describe.py
+++ describe.py
@@ -3,70 +3,35 @@ import sys
 from math import *
 
 class Describe:
-    def __init__(self):
+    def __init__(self, file):
+        try:
+            csv_reader = csv.DictReader(file)
+        except:
+            sys.exit("file is not csv")
         self.data = [] # csv file stored as an array of dicts
-        self.features_name = [] # list of str containing all the features name
-        self.numerical_features = [] # list of str contraining all the features name with numerical values
-        self.values = {}
-    
-    def get_values(self):
-        values = { # dict of dict for every caterogy so when can find the Std of Herbology at values["Std"]["Herbology"]
-            "Count":{item: 0.0 for item in self.numerical_features},
-            "Mean":{item: 0.0 for item in self.numerical_features},
-            "Std":{item: 0.0 for item in self.numerical_features},
-            "Min":{item: 0.0 for item in self.numerical_features},
-            "25%":{item: 0.0 for item in self.numerical_features},
-            "50%":{item: 0.0 for item in self.numerical_features},
-            "75%":{item: 0.0 for item in self.numerical_features},
-            "Max":{item: 0.0 for item in self.numerical_features}
-        }
-        values = self.get_describe_values(values)
-        return values
-
-    def parse(self, file_name):
-        with open(file_name) as csv_file: # store the csv file in self.data
-            reader = csv.DictReader(csv_file)
-            for line in reader:
-                self.data.append(line)
-        with open(file_name) as csv_file: # get the column names
-            self.features_name = str(list(csv_file)[0]).strip('\n').split(',')
-        self.find_numerical_columns()
-        return self.numerical_features
-
-    def find_numerical_columns(self): # function to get the names of the columns that contains numerical values
-        for tmp in self.features_name:
-            try:
-                float(self.data[0][tmp])
-                self.numerical_features.append(tmp)
-            except:
-                ()
-
-    def get_describe_values(self, values):
-        values = self.count(values)
-        values = self.mean(values)
-        values = self.std(values)
-        values = self.min(values)
-        values = self.percentile(values, 25) # TODO handle bonuses as input with other value than 25/50/75
-        values = self.percentile(values, 50) # TODO handle bonuses as input 
-        values = self.percentile(values, 75) # TODO handle bonuses as input 
-        values = self.max(values)
-        self.values = values
-        return values
+        self.numerical_data = []
+        for row in csv_reader:
+            self.data.append(row)
+            numerical_row = {}
+            for feature in row:
+                if feature == "First Name":
+                    continue
+                try:
+                    numerical_row[feature] = float(row[feature])
+                except:
+                    pass
+            self.numerical_data.append(numerical_row)
 
     def describe(self):
-        categories = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"] # list of all the categories that will be described
-        spacing = [] # list of numbers
-        values = { # dict of dict for every caterogy so when can find the Std of Herbology at values["Std"]["Herbology"]
-            "Count":{item: 0.0 for item in self.numerical_features},
-            "Mean":{item: 0.0 for item in self.numerical_features},
-            "Std":{item: 0.0 for item in self.numerical_features},
-            "Min":{item: 0.0 for item in self.numerical_features},
-            "25%":{item: 0.0 for item in self.numerical_features},
-            "50%":{item: 0.0 for item in self.numerical_features},
-            "75%":{item: 0.0 for item in self.numerical_features},
-            "Max":{item: 0.0 for item in self.numerical_features}
-        }
-        values = self.get_describe_values(values)
+        values = {}
+        values["Count"] = self.get_count_data()
+        values["Mean"] = self.get_mean_data(values["Count"])
+        values["Std"] = self.get_std_data(values["Count"], values["Mean"])
+        values["Min"] = self.get_min_data()
+        values["25%"] = self.get_percentile_data(25, values["Count"]) # TODO handle bonuses as input with other value than 25/50/75
+        values["50%"] = self.get_percentile_data(50, values["Count"]) # TODO handle bonuses as input 
+        values["75%"] = self.get_percentile_data(75, values["Count"]) # TODO handle bonuses as input 
+        values["Max"] = self.get_max_data()
         # First, print the header row
         header = "{:<15}".format("")  # Empty cell at the top left corner
         for category in values["Count"].keys():
@@ -97,119 +62,79 @@ class Describe:
         #         # print("{:<{}}".rjust(5), "{:.6f}".format(values[category][column]).rjust(len(category)), end="")
         #     print()
 
-    def count(self, values):
-        count_values = values["Count"]
-        track_count = {
-            item: 0 for item in self.numerical_features
-        }
-        for line in self.data:
-            for features in self.numerical_features:
-                try:
-                    if line[features] != '':
-                        track_count[features] += 1
-                except:
-                    ()
-        for features in self.numerical_features:
-            count_values[features] = track_count[features]
-        values["Count"] = count_values
-        return values
+    def get_count_data(self):
+        count_data = {}
+        for row in self.numerical_data:
+            for feature in row:
+                if feature not in count_data:
+                    count_data[feature] = 0
+                count_data[feature] += 1
+        return count_data
 
-    def mean(self, values): 
-        mean_values = values["Mean"]
-        track_mean = {
-            item: 0 for item in self.numerical_features
-        }
-        for line in self.data:
-            for features in self.numerical_features:
-                try:
-                    if line[features] != '':
-                        track_mean[features] += float(line[features])
-                except:
-                    ()
-        for features in self.numerical_features:
-            mean_values[features] = track_mean[features] / values["Count"][features]
-        values["Mean"] = mean_values
-        return values
+    def get_mean_data(self, count_data):
+        mean_data = {}
+        for row in self.numerical_data:
+            for feature in row:
+                if feature not in mean_data:
+                    mean_data[feature] = 0
+                mean_data[feature] += row[feature]
+        for features in mean_data:
+            mean_data[features] = mean_data[features] / count_data[features]
+        return mean_data
 
-    def std(self, values): 
-        std_values = values["Std"]
-        track_std = {
-            item: 0 for item in self.numerical_features
-        }
-        for line in self.data:
-            for features in self.numerical_features:
-                try:
-                    if line[features] != '':
-                        track_std[features] += (float(line[features]) - values["Mean"][features])**2
-                except:
-                    ()
-        for features in self.numerical_features:
-            std_values[features] = sqrt((track_std[features] / (values["Count"][features] - 1)))
-        values["Std"] = std_values
-        return values
+    def get_std_data(self, count_data, mean_data):
+        std_data = {}
+        for row in self.numerical_data:
+            for feature in row:
+                if feature not in std_data:
+                    std_data[feature] = 0
+                std_data[feature] += (row[feature] - mean_data[feature])**2
+        for features in std_data:
+            std_data[features] = sqrt(std_data[features] / (count_data[features] - 1))
+        return std_data
 
-    def min(self, values): 
-        min_values = values["Min"]
-        track_min = {
-            item: float('inf') for item in self.numerical_features
-        }
-        for line in self.data:
-            for features in self.numerical_features:
-                try:
-                    if line[features] != '' and float(line[features]) < track_min[features]:
-                        track_min[features] = float(line[features])
-                except:
-                    ()
-        for features in self.numerical_features:
-            min_values[features] = track_min[features]
-        values["Min"] = min_values
-        return values
+    def get_min_data(self):
+        min_data = {}
+        for row in self.numerical_data:
+            for feature in row:
+                if feature not in min_data or row[feature] < min_data[feature]:
+                    min_data[feature] = row[feature]
+        return min_data
 
-    def percentile(self, values, percentile):
-        percentile_values = values[str(percentile) + "%"]
-        values_ascendenting = {
-            item: [] for item in self.numerical_features
-        }
-        for line in self.data:
-            for features in self.numerical_features:
-                try:
-                    if line[features] != '':
-                        values_ascendenting[features].append(float(line[features]))
-                except:
-                    ()
-        for features in self.numerical_features:
-            values_ascendenting[features] = sorted(values_ascendenting[features])
-            percentile_index = int(percentile) / 100 * (values["Count"][features] - 1)
+    def get_percentile_data(self, percentile, count_data):
+        percentile_data = {}
+        for row in self.numerical_data:
+            for feature in row:
+                if feature not in percentile_data:
+                    percentile_data[feature] = []
+                percentile_data[feature].append(row[feature])
+        for feature in percentile_data:
+            percentile_data[feature] = sorted(percentile_data[feature])
+            percentile_index = (percentile) / 100 * (count_data[feature] - 1)
             index_offset = percentile_index - int(percentile_index)
             diff = 0
             if index_offset != 0:
-                diff = (values_ascendenting[features][int(percentile_index) + 1] - values_ascendenting[features][int(percentile_index)]) / 100 * (index_offset * 100) 
-            percentile_values[features] = values_ascendenting[features][int(percentile_index)] + diff
-        values[str(percentile) +"%"] = percentile_values
-        return values
+                diff = (percentile_data[feature][int(percentile_index) + 1] - percentile_data[feature][int(percentile_index)]) / 100 * (index_offset * 100) 
+            percentile_data[feature] = percentile_data[feature][int(percentile_index)] + diff
+        return percentile_data
 
-    def max(self, values): 
-        max_values = values["Max"]
-        track_max = {
-            item: float('-inf') for item in self.numerical_features
-        }
-        for line in self.data:
-            for features in self.numerical_features:
-                try:
-                    if line[features] != '' and float(line[features]) > track_max[features]:
-                        track_max[features] = float(line[features])
-                except:
-                    ()
-        for features in self.numerical_features:
-            max_values[features] = track_max[features]
-        values["Max"] = max_values
-        return values
-
-
+    def get_max_data(self): 
+        max_data = {}
+        for row in self.numerical_data:
+            for feature in row:
+                if feature not in max_data or row[feature] > max_data[feature]:
+                    max_data[feature] = row[feature]
+        return max_data
 
 def main():
-    d = Describe()
-    d.parse(sys.argv[1])
+    if (len(sys.argv) != 2):
+        sys.exit("wrong number of arguments")
+    file_name = sys.argv[1]
+    try:
+        with open(file_name, "r") as file:
+            d = Describe(file)
+    except:
+        sys.exit("can't open file")
     d.describe()
 
 if __name__ == "__main__":
