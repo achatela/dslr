8723b8bac87c2f700f19180e466764d69687bc0a
Hugo CARPENTIER <hcarpent@made-f0Ar8s7.clusters.42paris.fr>
2023-11-20 20:47:17 +0100

    Describe usable and histogram

--- describe.py
+++ describe.py
@@ -18,102 +18,102 @@ class Describe:
                 if re.fullmatch(r"[-+]?\d*\.?\d+", row[feature]):
                     numerical_row[feature] = float(row[feature])
             self.numerical_data.append(numerical_row)
+        self.stats = {}
+        self.stats["Count"] = self.calculate_features_count()
+        self.stats["Mean"] = self.calculate_features_mean(self.stats["Count"])
+        self.stats["Std"] = self.calculate_features_std(self.stats["Count"], self.stats["Mean"])
+        self.stats["Min"] = self.calculate_features_min()
+        self.stats["25%"] = self.calculate_features_percentile(25, self.stats["Count"]) # TODO handle bonuses as input with other value than 25/50/75
+        self.stats["50%"] = self.calculate_features_percentile(50, self.stats["Count"]) # TODO handle bonuses as input 
+        self.stats["75%"] = self.calculate_features_percentile(75, self.stats["Count"]) # TODO handle bonuses as input 
+        self.stats["Max"] = self.calculate_features_max()
 
     def describe(self):
         description = {}
-        count_values = self.get_count_values()
-        description["count"] = self.format_values(count_values)
-        mean_values = self.get_mean_values(count_values)
-        description["mean"] = self.format_values(mean_values)
-        description["std"] = self.format_values(self.get_std_values(count_values, mean_values))
-        description["min"] = self.format_values(self.get_min_values())
-        description["25%"] = self.format_values(self.get_percentile_values(25, count_values)) # TODO handle bonuses as input with other value than 25/50/75
-        description["50%"] = self.format_values(self.get_percentile_values(50, count_values)) # TODO handle bonuses as input 
-        description["75%"] = self.format_values(self.get_percentile_values(75, count_values)) # TODO handle bonuses as input 
-        description["max"] = self.format_values(self.get_max_values())
-
-        longest_values = {}
-        for statistic in description:
-            for feature in description[statistic]:
-                if feature not in longest_values:
-                    longest_values[feature] = len(feature)
-                if len(description[statistic][feature]) > longest_values[feature]:
-                    longest_values[feature] = len(description[statistic][feature])
-
-        featuresLine = " "*6
-        for feature in longest_values:
-            featuresLine += feature.rjust(longest_values[feature] + 1)
+        max_stat_len = 0
+        max_features_len = {}
+        for stat in self.stats:
+            description[stat] = {}
+            if len(stat) > max_stat_len:
+                max_stat_len = len(stat)
+            for feature in self.stats[stat]:
+                formatted_value = "{:.6f}".format(self.stats[stat][feature])
+                if feature not in max_features_len:
+                    max_features_len[feature] = len(feature)
+                if len(formatted_value) > max_features_len[feature]:
+                    max_features_len[feature] = len(formatted_value)
+                description[stat][feature] = formatted_value
+        featuresLine = " "*(max_stat_len + 1)
+        for feature in max_features_len:
+            featuresLine += feature.rjust(max_features_len[feature] + 4)
         print(featuresLine)
-        for statistic in description:
-            valuesLine = statistic.ljust(6)
-            for feature in description[statistic]:
-                valuesLine += description[statistic][feature].rjust(longest_values[feature] + 1)
+        for stat in description:
+            valuesLine = stat.ljust(max_stat_len + 1)
+            for feature in description[stat]:
+                valuesLine += description[stat][feature].rjust(max_features_len[feature] + 4)
             print(valuesLine)
 
-    def format_values(self, values):
-        return {feature : "{:.6f}".format(values[feature]) for feature in values}
-
-    def get_count_values(self):
-        count_values = {}
+    def calculate_features_count(self):
+        features_count = {}
         for row in self.numerical_data:
             for feature in row:
-                if feature not in count_values:
-                    count_values[feature] = 0
-                count_values[feature] += 1
-        return count_values
+                if feature not in features_count:
+                    features_count[feature] = 0
+                features_count[feature] += 1
+        return features_count
 
-    def get_mean_values(self, count_values):
-        mean_values = {}
+    def calculate_features_mean(self, features_count):
+        features_mean = {}
         for row in self.numerical_data:
             for feature in row:
-                if feature not in mean_values:
-                    mean_values[feature] = 0
-                mean_values[feature] += row[feature]
-        for features in mean_values:
-            mean_values[features] = mean_values[features] / count_values[features]
-        return mean_values
+                if feature not in features_mean:
+                    features_mean[feature] = 0
+                features_mean[feature] += row[feature]
+        for features in features_mean:
+            features_mean[features] = features_mean[features] / features_count[features]
+        return features_mean
 
-    def get_std_values(self, count_values, mean_values):
-        std_values = {}
+    def calculate_features_std(self, features_count, features_mean):
+        features_std = {}
         for row in self.numerical_data:
             for feature in row:
-                if feature not in std_values:
-                    std_values[feature] = 0
-                std_values[feature] += (row[feature] - mean_values[feature])**2
-        for features in std_values:
-            std_values[features] = sqrt(std_values[features] / (count_values[features] - 1))
-        return std_values
+                if feature not in features_std:
+                    features_std[feature] = 0
+                features_std[feature] += (row[feature] - features_mean[feature])**2
+        for features in features_std:
+            features_std[features] = sqrt(features_std[features] / (features_count[features] - 1))
+        return features_std
 
-    def get_min_values(self):
-        min_values = {}
+    def calculate_features_min(self):
+        features_min = {}
         for row in self.numerical_data:
             for feature in row:
-                if feature not in min_values or row[feature] < min_values[feature]:
-                    min_values[feature] = row[feature]
-        return min_values
+                if feature not in features_min or row[feature] < features_min[feature]:
+                    features_min[feature] = row[feature]
+        return features_min
 
-    def get_percentile_values(self, percentile, count_values):
-        percentile_values = {}
+    def calculate_features_percentile(self, percentile, features_count):
+        features_percentile = {}
         for row in self.numerical_data:
             for feature in row:
-                if feature not in percentile_values:
-                    percentile_values[feature] = []
-                percentile_values[feature].append(row[feature])
-        for feature in percentile_values:
-            percentile_values[feature] = sorted(percentile_values[feature])
-            percentile_index = percentile / 100 * (count_values[feature] - 1)
+                if feature not in features_percentile:
+                    features_percentile[feature] = []
+                features_percentile[feature].append(row[feature])
+        for feature in features_percentile:
+            features_percentile[feature] = sorted(features_percentile[feature])
+            percentile_index = percentile / 100 * (features_count[feature] - 1)
             index_offset = percentile_index - int(percentile_index)
-            diff = (percentile_values[feature][int(percentile_index) + 1] - percentile_values[feature][int(percentile_index)]) / 100 * (index_offset * 100)
-            percentile_values[feature] = percentile_values[feature][int(percentile_index)] + diff
-        return percentile_values
+            diff = (features_percentile[feature][int(percentile_index) + 1] - features_percentile[feature][int(percentile_index)]) / 100 * (index_offset * 100)
+            features_percentile[feature] = features_percentile[feature][int(percentile_index)] + diff
+        return features_percentile
 
-    def get_max_values(self): 
-        max_values = {}
+    def calculate_features_max(self): 
+        features_max = {}
         for row in self.numerical_data:
             for feature in row:
-                if feature not in max_values or row[feature] > max_values[feature]:
-                    max_values[feature] = row[feature]
-        return max_values
+                if feature not in features_max or row[feature] > features_max[feature]:
+                    features_max[feature] = row[feature]
+        return features_max
 
 def main():
     if (len(sys.argv) != 2):
--- histogram.py
+++ histogram.py
@@ -1,13 +1,26 @@
 from describe import Describe
-import sys
 import matplotlib.pyplot as plt
+import sys
 
 def main():
-    d = Describe()
-    numerical_features = d.parse(sys.argv[1])
-    print(numerical_features)
-    values = d.get_values()
-    # plt.plot(values["Std"])
+    if (len(sys.argv) != 2):
+        sys.exit("wrong number of arguments")
+    file_name = sys.argv[1]
+    try:
+        with open(file_name, "r") as file:
+            d = Describe(file)
+    except:
+        sys.exit("can't open file")
+    homogeneity = []
+    courses = []
+    for feature in d.stats["Std"]:
+        if feature != "Index":
+            homogeneity.append(d.stats["Std"][feature] / (d.stats["Max"][feature] - d.stats["Min"][feature]))
+            courses.append(feature)
+    plt.bar(range(len(courses)), homogeneity)
+    plt.xticks(range(len(courses)), courses)
+    plt.title("Courses Homogeneity")
+    plt.show()
 
 if __name__ == "__main__":
     main()
