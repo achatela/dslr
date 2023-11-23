b9340e333f491ad38c5c14b38e041c7632e14ebd
Alexis CHATELAIN <achatela@made-f0Ar11s6.clusters.42paris.fr>
2023-11-23 14:08:54 +0100

    certainement n'importe quoi

--- logreg_train.py
+++ logreg_train.py
@@ -1,14 +1,31 @@
 from describe import Describe
 import sys
+import numpy as np
+
+# Gryffindor 0
+# Slytherin 1
+# Ravenclaw 2
+# Hufflepuff 3
+
+def prediction(theta, grade):
+    Z = theta * grade
+    return float(1/(1 + np.exp(-Z)))
 
 def calculate_theta(feature_values):
-    theta = 0
-    L = 0.0000001 # Learning Rate
-
+    theta = 0.0
+    L = 0.00001 # Learning Rate
+    epochs = 100
     
+    for _ in range(epochs):
+        sums = 0.0
+        for line in feature_values:
+            sums += prediction(theta, float(line[1])) - float(line[0]) * float(line[1])
+        theta = theta - (L / len(feature_values)) * sums
+    print(theta)
     return theta
 
 def min_max_normalization(selected_features):
+    # return 0
     for feature in selected_features: 
         mini = min(selected_features[feature])[1]
         maxi = max(selected_features[feature])[1]
@@ -29,6 +46,16 @@ def write_to_txt(thetas, selected_features):
     f.write("\n")
     f.close()
 
+def get_house_value(str):
+    if str == "Gryffindor":
+        return 0.0
+    elif str == "Slytherin":
+        return 1.0
+    elif str == "Ravenclaw":
+        return 2.0
+    elif str == "Hufflepuff":
+        return 3.0
+
 def main():
     if (len(sys.argv) != 2):
         sys.exit("wrong number of arguments")
@@ -45,13 +72,14 @@ def main():
     for line in d.data:
         for feature in line:
             if feature in selected_features and line[feature] != '':
-                features_value[feature].append([line["Hogwarts House"], float(line[feature])])
-    min_max_normalization(features_value)
-    print(features_value)
+                features_value[feature].append([float(get_house_value(line["Hogwarts House"])), float(line[feature])])
+    # min_max_normalization(features_value)
+    # print(features_value)
     thetas = []
 
     for feature in selected_features:
         thetas.append(calculate_theta(features_value[feature]))
+    print(prediction(thetas[0], features_value["Astronomy"][0][1]))
     write_to_txt(thetas, selected_features)
 
 if __name__ == "__main__":
