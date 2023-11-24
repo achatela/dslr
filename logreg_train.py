1608df69b2b47c8bcdcb31818cefdb79c5cd407f
Alexis CHATELAIN <achatela@made-f0Ar2s3.clusters.42paris.fr>
2023-11-24 18:47:28 +0100

    training ~80% precision

--- logreg_train.py
+++ logreg_train.py
@@ -6,26 +6,78 @@ import numpy as np
 # Slytherin 1
 # Ravenclaw 2
 # Hufflepuff 3
+precision = 0
+overall_precision = 0
 
 def prediction(theta, grade):
     Z = theta * grade
     return float(1/(1 + np.exp(-Z)))
 
-def calculate_theta(feature_values):
-    theta = 0.0
-    L = 0.00001 # Learning Rate
-    epochs = 100
+def check_prediction(theta, grades, houses, answer):
+    global precision
+    global overall_precision
+
+    highest = 0
+    result  = str()
+    j = 0
+
+    gryff_prob = 0
+    raven_prob = 0
+    sly_prob = 0
+    puff_prob = 0
+
+    gryff_overall = 0
+    raven_overall = 0
+    sly_overall = 0
+    puff_overall = 0
+
+    for grade in grades:
+        for i in range(len(houses)):
+            tmp = prediction(theta[j][i], grade)
+            if i == 0:
+                gryff_prob = tmp
+            elif i == 1:
+                sly_prob = tmp
+            elif i == 2:
+                raven_prob = tmp
+            elif i == 3:
+                puff_prob = tmp
+                gryff_overall += gryff_prob
+                sly_overall += sly_prob
+                raven_overall += raven_prob
+                puff_overall += puff_prob
+                print("Probabilities for", "Gryff = ", gryff_prob, "Raven =", raven_prob, "Sly =", sly_prob, "Puff =", puff_prob)
+            if tmp > highest:
+                highest = tmp
+                result = houses[i]
+        j += 1
+        if result == answer:
+            precision += 1
+        print("Student is from", result)
+    if gryff_overall > sly_overall and gryff_overall > raven_overall and gryff_overall > puff_overall and result == "Gryffindor":
+        overall_precision += 1
+    if sly_overall > gryff_overall and sly_overall > raven_overall and sly_overall > puff_overall and result == "Slytherin":
+        overall_precision += 1
+    if raven_overall > sly_overall and raven_overall > gryff_overall and raven_overall > puff_overall and result == "Ravenclaw":
+        overall_precision += 1
+    if puff_overall > sly_overall and puff_overall > raven_overall and puff_overall > gryff_overall and result == "Hufflepuff":
+        overall_precision += 1
+    # print("Overall probabilities Gryff =", gryff_overall, "Sly =", sly_overall, "Raven =", raven_overall, "Puff =", puff_overall)
+
+def calculate_theta(feature_values, house):
+    theta = 1
+    L = 0.00000001 # Learning Rate
+    epochs = 10
     
-    # for _ in range(epochs):
-    #     sums = 0.0
-    #     for line in feature_values:
-    #         sums += prediction(theta, float(line[1])) - float(line[0]) * float(line[1])
-        # theta = theta - (L / len(feature_values)) * sums
     for _ in range(epochs):
         sums = 0.0
         for line in feature_values:
             h = prediction(theta, float(line[1]))
-            error = h - float(line[0])
+            if house != line[0]:
+                error = h + 1
+            else:
+                error = h
+            # error = h - float(line[0])
             sums += error * float(line[1])
         theta = theta - L * sums
     return theta
@@ -71,21 +123,35 @@ def main():
     except:
         sys.exit("can't open file")
     
-    selected_features = ["Astronomy", "Defense Against the Dark Arts", "Divination"]
+    houses = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
+    selected_features = ["Astronomy", "Defense Against the Dark Arts", "Divination", "Muggle Studies", "Charms"] # Muggle Studie, Charms
     features_value = {item: [] for item in selected_features}
     
     for line in d.data:
         for feature in line:
             if feature in selected_features and line[feature] != '':
-                features_value[feature].append([float(get_house_value(line["Hogwarts House"])), float(line[feature])])
+                features_value[feature].append([line["Hogwarts House"], float(line[feature])])
     min_max_normalization(features_value)
     # print(features_value)
-    thetas = []
+    thetas = {item: {} for item in houses}
 
     for feature in selected_features:
-        thetas.append(calculate_theta(features_value[feature]))
-    print(thetas)
-    write_to_txt(thetas, selected_features)
+        for house in houses:
+            thetas[house][feature] = calculate_theta(features_value[feature], house)
+    # write_to_txt(thetas, selected_features)
+    theta_list = []
+    i = 0
+    for feature in selected_features:
+        theta_list.append([])
+        for house in houses:
+            theta_list[i].append(thetas[house][feature])
+        i += 1
+
+    print(theta_list)
+    for i in range(200):
+        print(features_value["Astronomy"][i])
+        check_prediction(theta_list, [float(features_value["Astronomy"][i][1]), float(features_value["Defense Against the Dark Arts"][i][1]), float(features_value["Divination"][i][1]), float(features_value["Muggle Studies"][i][1]), float(features_value["Charms"][i][1])], houses, d.data[i]["Hogwarts House"])
 
 if __name__ == "__main__":
     main()
+    print("Precision =", precision, "Overall precison =", overall_precision)
